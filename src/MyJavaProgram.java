e8b351ec02af2e0238902d57504df7a9cce524bb
league <league@wts9.attlocal.net>
2018-03-06 16:59:35 -0800

    my java program

--- src/MyJavaProgram.java
+++ src/MyJavaProgram.java
@@ -1,4 +1,7 @@
 import java.awt.Color;
+
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyJavaProgram {
@@ -6,8 +9,14 @@ public class MyJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+	/*JOptionPane.showMessageDialog(null, "hello");
+	JOptionPane.showInputDialog("what's your favorite color");*/
+	String blue = "blue";
+	String red = "red";
+	String purple = red + blue;
+	JOptionPane.showMessageDialog(null, purple);
 	
-		
+	
 		
 	}
 }
